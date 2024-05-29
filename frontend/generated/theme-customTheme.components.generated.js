d2817c05773182a97b58ee97ab913ba23cb5b274
k.buglak <k.buglak@crestwavetech.ru>
2024-05-29 16:28:26 +0300

    next

---
+++ frontend/generated/theme-customTheme.components.generated.js
@@ -0,0 +1,14 @@
+
+
+
+if (!document['_vaadintheme_customTheme_componentCss']) {
+  
+  document['_vaadintheme_customTheme_componentCss'] = true;
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept((module) => {
+    window.location.reload();
+  });
+}
+
